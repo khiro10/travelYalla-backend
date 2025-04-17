83a7d12e504b200ac384bcc9f533fa6c0234d604
khireddine benchikh <65354259+khiro10@users.noreply.github.com>
2025-04-17 01:04:44 +0100

    Update index.js change all apis

--- index.js
+++ index.js
@@ -66,7 +66,28 @@ app.get("/api/countries", async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 });
+const key = "5ae2e3f221c38a28845f05b6f1871bf887b3a0f13b1539aa3e220ca7";
 
+app.get("/api/country/:id", async (req, res) => {
+    try {
+        const { origin, destination, currency } = req.query;
+        const response = await axios.get(`https://api.opentripmap.com/0.1/en/places/geoname?name=${req.params.id}&apikey=${key}`);
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+  
+});
+app.get("/api/countryplaces/:lat/:lon", async (req, res) => {
+     let radius = 30000; // 30 km radius 
+    try {
+        const { origin, destination, currency } = req.query;
+        const response = await axios.get(`https://api.opentripmap.com/0.1/en/places/radius?radius=${radius}&lon=${req.params.lon}&lat=${req.params.lat}&apikey=${key}`);
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
 app.get("/", (req, res) => {
   res.send("🌍 API is working!");
 });
